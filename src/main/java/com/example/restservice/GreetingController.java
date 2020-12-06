e2bbf3d4be932101e83e7c6804ae272d939bb040
Soupra-D <DS@Github974>
2020-12-06 17:15:20 +0100

    Add Get and Post Formulaire (basic stat)

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -1,19 +1,32 @@
 package com.example.restservice;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.Model.Formulaire;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+	private List<Formulaire> forms = new ArrayList<>();
+	private AtomicLong counter = new AtomicLong();
+	private AtomicLong nextId = new AtomicLong();
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+
+	@PostMapping("/formulaire")
+	public Formulaire creatFormulaire(@RequestBody Formulaire formulaire) {
+		//
+		formulaire.setId((int) nextId.incrementAndGet());
+		forms.add(formulaire);
+		return formulaire;
+	}
+
 }
